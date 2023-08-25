85ebc531f1e97de36bd93a20f864b845f0fdbb7e
Trinadh Thatakula <trinadh.thatakula@gmail.com>
2023-08-25 18:24:28 +0530

    New Thumbnails in WebP format

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "feb8216542aca0f9"; }
+/******/ 		__webpack_require__.h = function() { return "6b244bd88e08119b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
