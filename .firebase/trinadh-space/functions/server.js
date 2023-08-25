0a79c2a076f3170cd46ff4bd020349c411f86b1e
Trinadh Thatakula <trinadh.thatakula@gmail.com>
2023-08-25 18:50:04 +0530

    final commit for today

--- .firebase/trinadh-space/functions/server.js
+++ .firebase/trinadh-space/functions/server.js
@@ -1,4 +1,4 @@
 const { onRequest } = require('firebase-functions/v2/https');
   const server = import('firebase-frameworks');
-  exports.ssrtrinadhspace = onRequest({"region":"asia-east1","minInstances":0,"maxInstances":10}, (req, res) => server.then(it => it.handle(req, res)));
+  exports.ssrtrinadhspace = onRequest({"region":"asia-east1"}, (req, res) => server.then(it => it.handle(req, res)));
   
